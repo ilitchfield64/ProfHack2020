32ae825a6cfbfeb515fe207549ecef148de44a9e
Ian Litchfield <ilitchfield64@gmail.com>
2020-02-29 13:57:02 -0500

    Update ProfHack2020.java
    Package was named wrong

--- src/profhack2020/ProfHack2020.java
+++ src/profhack2020/ProfHack2020.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ProfHack2020;
+package profhack2020;
 
 import java.awt.Color;
 import java.awt.Dimension;
